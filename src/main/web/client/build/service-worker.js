750457ce3ed0288e075639e238ba5fa3730c798a
vikumarsing <vikumarsing@synlabs.io>
2019-12-10 18:30:54 +0530

    build files

--- src/main/web/client/build/service-worker.js
+++ src/main/web/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52653e79221574a382bd52b17401c454.js"
+  "/precache-manifest.fd628c85d6a0a747cd25f93cac41a852.js"
 );
 
 self.addEventListener('message', (event) => {
