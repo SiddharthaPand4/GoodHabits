d63ef2958e4c49949f3446116fd5b2c27da57b8a
vikumarsing <vikumarsing@synlabs.io>
2019-12-10 09:40:34 +0530

    Incident page done

--- src/main/web/client/src/views/TrafficIncidentView.js
+++ src/main/web/client/src/views/TrafficIncidentView.js
@@ -11,7 +11,7 @@ import {
     Table,
     Tag,
     Modal,
-    message, Input,Button
+    message, Input, Button, Menu, Dropdown, Typography
 } from 'antd';
 import GenericFilter from "../components/GenericFilter";
 import Moment from "react-moment";
@@ -19,6 +19,7 @@ import AnprService from "../services/AnprService";
 
 const {Column} = Table;
 const {Panel} = Collapse;
+const {Paragraph, Text} = Typography;
 
 
 export default class TrafficIncidentView extends Component {
@@ -31,8 +32,10 @@ export default class TrafficIncidentView extends Component {
             events: {},
             filter: {
                 page: 1,
-                pageSize: 10
-            }
+                pageSize: 24
+            },
+            workingEvent: {},
+            workingEventLoading: false
         };
 
         this.refresh = this.refresh.bind(this);
@@ -42,6 +45,8 @@ export default class TrafficIncidentView extends Component {
         this.onPageChange = this.onPageChange.bind(this);
         this.onPageSizeChange = this.onPageSizeChange.bind(this);
         this.onLprInputChange = this.onLprInputChange.bind(this);
+        this.editEvent = this.editEvent.bind(this);
+        this.updateEvent = this.updateEvent.bind(this);
     }
 
     componentDidMount() {
@@ -72,33 +77,56 @@ export default class TrafficIncidentView extends Component {
         let filter = this.state.filter;
         filter.lpr = e.target.value;
         console.log(filter);
-        this.setState({filter:filter})
+        this.setState({filter: filter})
     }
 
-    handleFilterChange(data){
-        this.setState({filter:data})
+    handleFilterChange(data) {
+        this.setState({filter: data})
     }
 
-     handleLayoutChange(data){
-         this.setState({layout:data})
-     }
+    handleLayoutChange(data) {
+        this.setState({layout: data})
+    }
 
-     handleRefresh(){
+    handleRefresh() {
         this.refresh();
-     }
+    }
 
-     onPageChange(page, pageSize) {
-         let filter = this.state.filter;
-         filter.page = page;
-         filter.pageSize = pageSize;
-         this.refreshNow(filter)
-     }
+    onPageChange(page, pageSize) {
+        let filter = this.state.filter;
+        filter.page = page;
+        filter.pageSize = pageSize;
+        this.refreshNow(filter)
+    }
 
-     onPageSizeChange(current, pageSize) {
-         let filter = this.state.filter;
-         filter.pageSize = pageSize;
-         this.refreshNow(filter);
-     }
+    onPageSizeChange(current, pageSize) {
+        let filter = this.state.filter;
+        filter.pageSize = pageSize;
+        this.refreshNow(filter);
+    }
+
+    editEvent(event) {
+        this.setState({workingEvent: event});
+    }
+
+    updateEvent(anprText) {
+
+        let {workingEvent, workingEventLoading} = this.state;
+        workingEvent.anprText = anprText;
+        workingEventLoading = true;
+        this.setState({workingEvent, workingEventLoading});
+        AnprService.updateEvent(workingEvent).then(request => {
+            let {workingEvent, workingEventLoading} = this.state;
+            workingEvent.anprText = anprText;
+            workingEventLoading = false;
+            this.setState({workingEventLoading});
+        }).catch(error => {
+            alert("error in saving");
+            let {workingEventLoading} = this.state;
+            workingEventLoading = false;
+            this.setState({workingEventLoading});
+        })
+    }
 
 
     render() {
@@ -109,11 +137,13 @@ export default class TrafficIncidentView extends Component {
         return (<div>Incidents
             <Collapse bordered={false} defaultActiveKey={['1', '2']}>
                 <Panel header="Filter" key="1">
-                    LPR: <Input value={lpr} style={{ "width": "200px"}} onChange={this.onLprInputChange}/> <br/><br/>
-                    <GenericFilter handleRefresh={this.refresh} filter={this.state.filter} layout={layout} handleFilterChange={this.handleFilterChange} handleLayoutChange={this.handleLayoutChange}/>
+                    LPR: <Input value={lpr} style={{"width": "200px"}} onChange={this.onLprInputChange}/> <br/><br/>
+                    <GenericFilter handleRefresh={this.refresh} filter={this.state.filter} layout={layout}
+                                   handleFilterChange={this.handleFilterChange}
+                                   handleLayoutChange={this.handleLayoutChange}/>
                 </Panel>
                 <Panel header="Incidents" key="2">
-                    {layout === "table" ? (this.renderTable()) :(this.renderGrid()) }
+                    {layout === "table" ? (this.renderTable()) : (this.renderGrid())}
                 </Panel>
             </Collapse>
         </div>)
@@ -121,66 +151,91 @@ export default class TrafficIncidentView extends Component {
 
     renderGrid() {
 
+
         if (this.state.loading || !this.state.anprresponse || this.state.anprresponse.totalPage === 0) {
             return <Empty description={false}/>
         }
 
         let events = this.state.anprresponse.events;
-        let count =  this.state.anprresponse.totalPages *  this.state.anprresponse.pageSize;
+        let workingEventLoading = this.state.workingEventLoading;
+        let workingEvent = this.state.workingEvent;
+        let count = this.state.anprresponse.totalPages * this.state.anprresponse.pageSize;
 
         return <div style={{background: '#ECECEC', padding: '30px'}}>
             <Row>
-                <Col>
-                    <Pagination onChange={this.onPageChange} onShowSizeChange={this.onPageSizeChange} showSizeChanger showQuickJumper
-                              defaultCurrent={1}  total={count} current={this.state.filter.page} pageSize ={this.state.filter.pageSize}/>
-                </Col>
-            </Row>
-
-            <Row gutter={16}>
                 {
                     events.map((event, index) =>
-                        <Col span={8} key={index}>
+                        <Col xl={{span: 8}} lg={{span: 12}} md={{span: 16}} sm={{span: 20}} xs={{span: 20}} key={index}>
                             <Card
+                                style={{margin: "5px"}}
                                 title={
                                     <div>
-                                        <Tag color="#2db7f5"><Moment format="L">{event.eventDate}</Moment></Tag>
-                                        <Tag color="#87d068"><span><Moment format="LTS">{event.eventDate}</Moment></span><Icon
-                                            type="right" hidden/><span hidden><Moment
-                                            format="LTS">{event.eventDate}</Moment></span></Tag>
-                                        <Tag>{event.anprText}</Tag>
-                                        <Tag>{event.direction}</Tag>
-                                        {event.helmet ? <Tag><span>Helmet:No</span></Tag> : <span>&nbsp;</span>}
+                                        {(event.direction && event.direction === "rev") ?
+                                            <Tag color="#f50">Reverse</Tag> : null}
+                                        {(event.helmet) ? <Tag color="#f50">Wihtout helmet</Tag> : null}
                                     </div>
                                 }
+                                extra={<Dropdown overlay={<Menu>
+                                    <Menu.Item key="1">
+                                        <a
+                                            title={"click here to download"}
+                                            href={"/public/anpr/vehicle/" + event.id + "/image.jpg"}
+                                            download={true}><Icon type="download"/>{' '} Full
+                                            image</a>
+                                    </Menu.Item>
+                                    <Menu.Item key="2">
+                                        <a
+                                            title={"click here to download"}
+                                            href={"/public/anpr/lpr/" + event.id + "/image.jpg"}
+                                            download={true}><Icon type="download"/>{' '} Cropped image</a>
+                                    </Menu.Item>
+                                    <Menu.Item key="3">
+                                        <Button type="danger" onClick={() => this.archiveEvent(event)}><Icon
+                                            type="delete"/>{' '}
+                                            Delete
+                                        </Button>
+                                    </Menu.Item>
+
+                                </Menu>}>
+                                    <Button>
+                                        More <Icon type="down"/>
+                                    </Button>
+                                </Dropdown>}
                                 bordered={true}
-                                cover={<img alt="event"
-                                            src={"/public/anpr/vehicle/" + event.id + "/image.jpg"}/>}
+                                cover={
+                                    <img alt="event"
+                                         src={"/public/anpr/vehicle/" + event.id + "/image.jpg"}/>}
                             >
                                 <div style={{textAlign: "center"}}>
                                     <img alt="event"
                                          src={"/public/anpr/lpr/" + event.id + "/image.jpg"}/>
                                 </div>
+                                <div style={{marginTop: "5px", textAlign: "center"}}
+                                     onClick={() => this.editEvent(event)}>
+                                    <Paragraph
+                                        strong
+                                        editable={{onChange: this.updateEvent}}
+                                        copyable>{event.anprText}</Paragraph>
+                                    <Text
+                                        type="secondary">{(workingEventLoading && workingEvent.id === event.id) ? "saving..." : ""}</Text>
+                                    <div>
+                                        <Text code> <Moment format="L">{event.eventDate}</Moment>{' '}|{' '}<Moment
+                                            format="LTS">{event.eventDate}</Moment></Text>
+                                    </div>
+                                </div>
 
-                                <Button.Group>
-
-                                    <Button type="primary" icon="download" size={"small"}><a
-                                        title={"click here to download"}
-                                        href={"/public/anpr/vehicle/" + event.id + "/image.jpg"}
-                                        download={true} style={{color: "white"}}> Full image</a></Button>
-                                    <Button type="primary" icon="download" size={"small"}><a
-                                        title={"click here to download"}
-                                        href={"/public/anpr/lpr/" + event.id + "/image.jpg"}
-                                        download={true} style={{color: "white"}}> Cropped image</a>
-                                    </Button>
-                                    <Button type="danger" size={"small"} onClick={() => this.archiveEvent(event)}><Icon
-                                        type="warning"/>{' '}
-                                        Archive</Button>
-                                </Button.Group>
                             </Card>
                         </Col>
                     )
                 }
             </Row>
+            <div style={{textAlign: "right"}}>
+                <Pagination onChange={this.onPageChange} onShowSizeChange={this.onPageSizeChange} showSizeChanger
+                            showQuickJumper
+                            defaultCurrent={1} total={count} current={this.state.filter.page}
+                            pageSize={this.state.filter.pageSize}/>
+            </div>
+
         </div>
     }
 
@@ -191,7 +246,7 @@ export default class TrafficIncidentView extends Component {
         }
 
         let events = this.state.anprresponse.events;
-        let count = this.state.anprresponse.totalPages *  this.state.anprresponse.pageSize;
+        let count = this.state.anprresponse.totalPages * this.state.anprresponse.pageSize;
 
         const paginationOptions = {
             showSizeChanger: true,
@@ -219,10 +274,11 @@ export default class TrafficIncidentView extends Component {
                 <Column title="LPR" dataIndex="anprText" key="anprText"
                         render={anprText => anprText}/>
                 <Column title="image" dataIndex="id" key="anprimage"
-                        render={id => (<a title={"click here to download"} href={"/public/anpr/lpr/" + id + "/image.jpg"}
-                                          download={true}>
-                            <img alt="event"
-                                 src={"/public/anpr/lpr/" + id + "/image.jpg"}/></a>)}/>
+                        render={id => (
+                            <a title={"click here to download"} href={"/public/anpr/lpr/" + id + "/image.jpg"}
+                               download={true}>
+                                <img alt="event"
+                                     src={"/public/anpr/lpr/" + id + "/image.jpg"}/></a>)}/>
                 <Column title="direction" dataIndex="direction" key="direction"
                         render={direction => direction}/>
                 <Column title="Helmet" dataIndex="helmet" key="helmet"
@@ -230,8 +286,8 @@ export default class TrafficIncidentView extends Component {
                 <Column title="Action"
                         key="action"
                         render={(text, event) => (
-                            <Button type="danger" onClick={() => this.archiveEvent(event)}><Icon type="warning"/>{' '}
-                                Archive</Button>
+                            <Button type="danger" onClick={() => this.archiveEvent(event)}><Icon type="delete"/>{' '}
+                                Delete</Button>
                         )}
                 />
             </Table>
