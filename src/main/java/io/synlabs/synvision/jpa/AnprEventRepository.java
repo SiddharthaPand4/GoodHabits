6941669f221984a9f3be639832fa8cd8a79732e9
vikumarsing <vikumarsing@synlabs.io>
2020-01-13 12:16:17 +0530

    Query-> db name removed

--- src/main/java/io/synlabs/synvision/jpa/AnprEventRepository.java
+++ src/main/java/io/synlabs/synvision/jpa/AnprEventRepository.java
@@ -24,9 +24,9 @@ public interface AnprEventRepository extends JpaRepository<AnprEvent, Long>, Que
 
     List<AnprEvent> findAllByArchivedFalse(Pageable paging);
 
-    @Query(nativeQuery = true, value = "SELECT count(*) FROM (SELECT anpr_text, count(anpr_text) as count FROM synvision.anpr_event where direction = 'rev' group by anpr_text having count > 1) as x ")
+    @Query(nativeQuery = true, value = "SELECT count(*) FROM (SELECT anpr_text, count(anpr_text) as count FROM anpr_event where direction = 'rev' group by anpr_text having count > 1) as x ")
     public long countReverseDirectionRepeatedIncidents();
 
-    @Query(nativeQuery = true, value = "SELECT count(*) FROM (SELECT anpr_text, count(anpr_text) as count FROM synvision.anpr_event where helmet_missing = b'1' group by anpr_text having count > 1) as x  ")
+    @Query(nativeQuery = true, value = "SELECT count(*) FROM (SELECT anpr_text, count(anpr_text) as count FROM anpr_event where helmet_missing = b'1' group by anpr_text having count > 1) as x  ")
     public long countHelmetMissingRepeatedIncidents();
 }
