305b04e8d1d4b5ed61c36c0338f3efb0cd07bc91
karandeep singh <karan.deep@synlabs.io>
2020-01-16 18:09:36 +0530

    Master-Data added : Archive option added

--- src/main/java/io/synlabs/synvision/jpa/ApcEventRepository.java
+++ src/main/java/io/synlabs/synvision/jpa/ApcEventRepository.java
@@ -4,7 +4,7 @@ import io.synlabs.synvision.entity.apc.ApcEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface ApcEventRepository extends JpaRepository<ApcEvent, Long>, QuerydslPredicateExecutor<ApcEvent> {
+public interface ApcEventRepository extends JpaRepository<ApcEvent, Long> {
 
 
 }
