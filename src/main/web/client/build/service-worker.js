63db2463f68ab70be52077c10915b877b65a6158
vikumarsing <vikumarsing@synlabs.io>
2020-02-06 13:12:24 +0530

    Dashboard color

--- src/main/web/client/build/service-worker.js
+++ src/main/web/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f219174d3501572a67e5ef9d2fc47711.js"
+  "/precache-manifest.7b43b98db5ba5ec3dfd4a49a74bbe7ef.js"
 );
 
 self.addEventListener('message', (event) => {
