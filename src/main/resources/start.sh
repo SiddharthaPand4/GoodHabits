8e7d16305eb6a5b7cebe141080025c140af5a248
vikumarsing <vikumarsing@synlabs.io>
2020-04-10 18:28:32 +0530

    Start script and new build

--- src/main/resources/start.sh
+++ src/main/resources/start.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
+sudo fuser -k 8080/tcp
 nohup java -DRDS_HOSTNAME=synlabs-db-cluster-do-user-6775492-0.a.db.ondigitalocean.com -DRDS_PORT=25060 -DRDS_DB_NAME=defaultdb -DRDS_USERNAME=doadmin -DRDS_PASSWORD=h8olnfl80thfurla -Dserver.port=80 -Duser.timezone=IST -Dspring.profiles.active=prod -jar /home/synvision/synvision-9.jar
