3d43a7e3ff3ca1f8a367a31cc015b5a2563520d4
vikumarsing <vikumarsing@synlabs.io>
2020-05-14 15:49:12 +0530

    redirect to index.html

--- src/main/java/io/synlabs/synvision/controller/MainController.java
+++ src/main/java/io/synlabs/synvision/controller/MainController.java
@@ -1,27 +1,24 @@
 package io.synlabs.synvision.controller;
 
-import io.synlabs.synvision.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @CrossOrigin(origins = "http://localhost:3000")
-@Controller
+@RestController
 public class MainController {
 
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
-
-    @Autowired
-    private UserService userService;
-
-    @GetMapping("/")
-    public String index(Model model) {
-        return "index";
+    @RequestMapping(value = "/")
+    void index(HttpServletResponse response) throws IOException {
+        response.sendRedirect("index.html");
     }
 
 }
