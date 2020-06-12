81ec815a99420018226964dd2797626666e55333
karandeep singh <karan.deep@synlabs.io>
2020-06-12 15:07:08 +0530

    view improved

--- src/main/web/client/src/views/Polygon/AnnotationView.js
+++ src/main/web/client/src/views/Polygon/AnnotationView.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {Button, Col, message} from "antd";
+import {Button, Col,Icon, message} from "antd";
 import AnnotationService from "../../services/AnnotationService";
 import Row from "antd/es/grid/row";
 import Card from "@material-ui/core/Card";
+
 
 
 export default class AnnotationView extends Component {
@@ -32,20 +33,22 @@ export default class AnnotationView extends Component {
         this.drawArrow = this.drawArrow.bind(this);
         this.drawLine = this.drawLine.bind(this);
         this.capture = this.capture.bind(this);
+        this.refressh=this.refresh.bind(this);
 
     }
+
 //To store image use this.
 
-      /*save = ()=>{
-         AnnotationService.saveAnnotation(this.state.lines,this.state.dataURL)
-             .then( res => {
-                 message.success("Annotation Saved")
-                 // this.setState({lines:null})
-             }).catch( err => {
+    /*save = ()=>{
+       AnnotationService.saveAnnotation(this.state.lines,this.state.dataURL)
+           .then( res => {
+               message.success("Annotation Saved")
+               // this.setState({lines:null})
+           }).catch( err => {
 
-             message.error("Something went wrong")
-         })
-     }*/
+           message.error("Something went wrong")
+       })
+   }*/
 
     save = () => {
 
@@ -61,26 +64,14 @@ export default class AnnotationView extends Component {
 
 
     }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        this.drawLines();
-    }
-
-    drawLines() {
-        const canvas = this.refs.canvas;
-        var ctx = canvas.getContext("2d");
-        this.state.lines.map(line => {
-            ctx.moveTo(line.x1 * this.state.canvasWidth, line.y1 * this.state.canvasHeight);
-            ctx.lineTo(line.x2 * this.state.canvasWidth, line.y2 * this.state.canvasHeight);
-            ctx.stroke();
-        });
+    refresh(){
 
     }
 
     startFeed = () => {
         AnnotationService.startFeed().then(res => {
             message.success("Feed started")
-            //this.history.push("/canvasview")
+            window.location.reload();
 
         }).catch(err => {
             message.error("Something Went Wrong!")
@@ -89,117 +80,21 @@ export default class AnnotationView extends Component {
     stopFeed = () => {
         AnnotationService.stopFeed().then(res => {
             message.success("Feed stoped")
-            //this.history.push("/canvasview")
+
         }).catch(err => {
             message.error("Something Went Wrong!")
         })
     }
 
-    render() {
-        let isPlaying = this.state.isPlaying;
-        let arrow = this.state.arrow;
-        return (
-            <div>
-
-                <Card>
-                    <Row>
-                        <Col span={12}>
-                            {isPlaying
-                                ? <img id="video" controls width="500" height="260" src="/pgs/p-001.png"></img>
-                                :
-                                <img id="video" controls width="500" height="260" src="http://localhost:9000/ss"></img>
-                            }
-                            <br/><br/><Button style={{width: "500px"}} type="primary"
-                                              onClick={() => this.setState({isPlaying: !isPlaying})}>PLAY/PAUSE</Button>
-                            <br/><br/><Button style={{width: "500px"}} type="primary" block
-                                              onClick={this.capture}>Capture</Button>
-                        </Col>
-
-                        <Col span={12}>
-                            <canvas id="canvas" ref="canvas"
-                                    width={500}
-                                    height={278}
-
-                                    allowTaint="true"
-                                    onMouseDown={
-                                        e => {
-                                            let nativeEvent = e.nativeEvent;
-                                            this.handleMouseDown(nativeEvent);
-                                        }}
-                                    onMouseMove={
-                                        e => {
-                                            let nativeEvent = e.nativeEvent;
-                                            this.handleMouseMove(nativeEvent);
-                                        }}
-                                    onMouseUp={
-                                        e => {
-                                            let nativeEvent = e.nativeEvent;
-                                            if (arrow === false) {
-                                                this.handleMouseUp(nativeEvent);
-                                            } else {
-                                                this.handleArrow(nativeEvent);
-                                            }
-                                        }}
-
-                            />
-                        </Col>
-                    </Row>
-                </Card>
-
-                <div align={"right"}>
-                    <Button onClick={this.drawLine}>line</Button>
-                    <Button onClick={this.drawArrow}>arrow</Button>
-                    <Button onClick={this.clearAll}>Clear All</Button>
-                    <Button onClick={this.save}>Save</Button></div>
-                <Button onClick={this.startFeed}>Start Feed</Button>
-                <Button onClick={this.stopFeed}>Stop Feed</Button>
-
-            </div>
-        );
+    componentDidMount() {
+        this.drawLines();
     }
 
-    drawArrow() {
-        this.setState({
-            arrow: true
-        })
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.drawLines();
     }
 
-    drawLine() {
-        this.setState({
-            arrow: false
-        })
-    }
-
-    clearAll = () => {
-
-        this.setState({
-            previousPointX: "",
-            previousPointY: "",
-            lines: []
-        })
-        const canvas = this.refs.canvas;
-        var ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.beginPath();
-
-        // var canvas = ReactDOM.findDOMNode(this.refs.canvas),
-        // ctx = canvas.getContext("2d");
-//
-        // canvas.width = this.state.canvasWidth;
-        // canvas.height = this.state.canvasHeight;
-//
-//
-        // var background = new Image();
-        // background.src = "/pgs/p-001.png";
-//
-// Make// sure the image is loaded first otherwise nothing will draw.
-        // background.onload = function () {
-        //     ctx.drawImage(background, 0, 0);
-        // }
-    }
-
-
-    handleMouseDown(event) { //added code here
+    handleMouseDown(event) {
         console.log(event);
         this.setState({
             isDown: true,
@@ -212,10 +107,7 @@ export default class AnnotationView extends Component {
             var ctx = canvas.getContext("2d");
 
             console.log(x, y);
-            // ctx.moveTo(x, y);
-            //
-            // ctx.lineTo(x + 1, y + 1);
-            // ctx.stroke();
+
         })
     }
 
@@ -223,12 +115,11 @@ export default class AnnotationView extends Component {
         let isDown = this.state.isDown;
         if (isDown === true) {
 
-
         }
     }
 
     handleMouseUp(event) {
-//if(this.state.isDown){
+
         var x = event.offsetX;
         var y = event.offsetY;
 
@@ -240,18 +131,9 @@ export default class AnnotationView extends Component {
         let prevY = this.state.previousPointY;
         let len = Math.sqrt(Math.pow(x - prevX, 2) + Math.pow((y - prevY), 2));
         if (len < 30) {
-            //ctx.clearRect(0, 0, canvas.width, canvas.height);
-            //ctx.beginPath();
+
             return;
         }
-
-        // ctx.moveTo(prevX, prevY);
-        // ctx.lineTo(x, y);
-
-        // //common
-        // ctx.stroke();
-        // ctx.strokeStyle = "#FF0000";
-        // ctx.closePath();
 
         let line = {
             x1: prevX / this.state.canvasWidth,
@@ -282,7 +164,7 @@ export default class AnnotationView extends Component {
 
         let len = Math.sqrt(Math.pow(x - x2, 2) + Math.pow((y - y2), 2));
         if (len < 25) {
-            //ctx.clearRect(0, 0, canvas.width, canvas.height);
+
             ctx.beginPath();
             return;
         }
@@ -297,10 +179,7 @@ export default class AnnotationView extends Component {
         var dx = x - x2;
         var dy = y - y2;
         var angle = Math.atan2(dy, dx);
-        // for arrow
-        // ctx.beginPath();
-        // ctx.moveTo(x2, y2);
-        // ctx.lineTo(x, y);
+
 
         let arrowX1 = x - headlen * Math.cos(angle + Math.PI / 6);
         let arrowY1 = y - headlen * Math.sin(angle + Math.PI / 6);
@@ -318,43 +197,136 @@ export default class AnnotationView extends Component {
             x2: arrowX2 / this.state.canvasWidth,
             y2: arrowY2 / this.state.canvasHeight
         });
-        //  ctx.lineTo(arrowX1, arrowY1);
-
-        //  ctx.moveTo(x, y);
-
-        //  ctx.lineTo(arrowX2, arrowY2)
-
-        //  //common
-        //  ctx.stroke();
-        //  ctx.strokeStyle = "#FF0000";
-        //  ctx.closePath();
 
         this.setState({
             isDown: false,
             lines: lines
         });
-//}
+
+    }
+    drawArrow() {
+        this.setState({
+            arrow: true
+        })
     }
 
+    drawLine() {
+        this.setState({
+            arrow: false
+        })
+    }
+
+    clearAll = () => {
+
+        this.setState({
+            previousPointX: "",
+            previousPointY: "",
+            lines: []
+        })
+        const canvas = this.refs.canvas;
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+
+    }
     capture() {
 
-        let dataURL = this.state.dataURL;
+        // let dataURL = this.state.dataURL;
         this.clearAll();
-        //var canvas = document.getElementById('canvas');
+
         var video = document.getElementById('video');
         var canvas = this.refs.canvas;
         canvas.getContext('2d').drawImage(video, 0, 0, 500, 278);
-        //this.clearAll();
-        // dataURL = canvas.toDataURL();
+
         this.setState({
             lines: [],
             // dataURL:dataURL
         })
 
+    }
+
+    drawLines() {
+        const canvas = this.refs.canvas;
+        var ctx = canvas.getContext("2d");
+        this.state.lines.map(line => {
+            ctx.moveTo(line.x1 * this.state.canvasWidth, line.y1 * this.state.canvasHeight);
+            ctx.lineTo(line.x2 * this.state.canvasWidth, line.y2 * this.state.canvasHeight);
+            ctx.stroke();
+            ctx.strokeStyle = "red";
+        });
 
     }
 
-    componentDidMount() {
-        this.drawLines();
+
+
+    render() {
+        let isPlaying = this.state.isPlaying;
+        let arrow = this.state.arrow;
+        return (
+            <div>
+
+                <Card>
+                    <Row>
+
+                        <Col span={12}>
+                            <br/><br/>&nbsp;&nbsp;<Button  onClick={this.startFeed}><Icon type="play-circle" />Start Feed</Button>
+                            &nbsp;&nbsp;<Button onClick={this.stopFeed}><Icon type="pause-circle" />Stop Feed</Button><br/><br/>
+                            {isPlaying
+                                ? <img style={{border:"1px solid black"}} id="video" controls width="500" height="260" src="http://localhost:9000/stream"></img>
+                                :
+                                <img style={{border:"1px solid black"}} id="video" controls width="500" height="260" src="http://localhost:9000/ss"></img>
+                            }
+                            <br/><br/><Button style={{width: "500px"}} type="primary"
+                                              onClick={() => this.setState({isPlaying: !isPlaying})}>PLAY/PAUSE</Button>
+                            <br/><br/><Button style={{width: "500px"}} type="primary" block
+                                              onClick={this.capture}>Capture</Button><br/><br/>
+                        </Col>
+
+                        <Col  span={12}>
+                            <br/><br/>
+
+                            <h3>Draw by clicking & moving the mouse !</h3>
+                            <canvas id="canvas" ref="canvas"
+                                    style={{border:"1px solid black"}}
+                                    width={500}
+                                    height={278}
+                                    border="2x solid blue"
+                                    allowTaint="true"
+                                    onMouseDown={
+                                        e => {
+                                            let nativeEvent = e.nativeEvent;
+                                            this.handleMouseDown(nativeEvent);
+                                        }}
+                                    onMouseMove={
+                                        e => {
+                                            let nativeEvent = e.nativeEvent;
+                                            this.handleMouseMove(nativeEvent);
+                                        }}
+                                    onMouseUp={
+                                        e => {
+                                            let nativeEvent = e.nativeEvent;
+                                            if (arrow === false) {
+                                                this.handleMouseUp(nativeEvent);
+                                            } else {
+                                                this.handleArrow(nativeEvent);
+                                            }
+                                        }}
+
+                            />
+                            <div align={"center"}>
+                                <h4>Draw using:</h4><Button onClick={this.drawLine}>line</Button>
+                                &nbsp;&nbsp;<Button onClick={this.drawArrow}>arrow</Button><br/><br/>
+                            </div>
+                                <div align={"right"}> <Button block type={"primary"}  onClick={this.save}>Save</Button>
+                                &nbsp;&nbsp;<Button block onClick={this.clearAll}>Clear All</Button><br/><br/>
+                                </div>
+                        </Col>
+                    </Row>
+                </Card>
+
+            </div>
+        );
     }
+
+
 }
