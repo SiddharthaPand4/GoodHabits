74b0a972ab899f27b0bed49243c8ba3f5ec85985
Sangam Dalal <sangam.dalal@synlabs.io>
2020-08-06 16:46:01 +0530

    minor fixes

--- src/main/java/io/synlabs/synvision/service/DataDeleteScheduler.java
+++ src/main/java/io/synlabs/synvision/service/DataDeleteScheduler.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.ZoneId;
+
 @Component
 public class DataDeleteScheduler {
 
@@ -33,41 +35,34 @@ public class DataDeleteScheduler {
 
     private static final Logger logger = LoggerFactory.getLogger(DataDeleteScheduler.class);
 
-    public DataDeleteScheduler(FileStorageProperties fileStorageProperties){
-        this.fileStorageProperties=fileStorageProperties;
+    public DataDeleteScheduler(FileStorageProperties fileStorageProperties) {
+        this.fileStorageProperties = fileStorageProperties;
     }
 
     @Scheduled(cron = "0 0 0 * * *")
-    public void DeleteFilesAfterNDays()
-    {
-
-        Path fileStorageLocation= Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath().normalize();
+    public void DeleteFilesAfterNDays() {
+        Path fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath().normalize();
         final File directory = new File(fileStorageLocation.toString());
-        if(directory.exists())
-        {
+        if (directory.exists()) {
             File[] listFiles = directory.listFiles();
-            for(File listFile:listFiles)
-            {
-                if(listFile.isDirectory())
-                {
-                    File[] subFiles=listFile.listFiles();
-                    for(File subFile :subFiles)
-                    {
-                        LocalDate lastModified=null;
+            for (File listFile : listFiles) {
+                if (listFile.isDirectory()) {
+                    File[] subFiles = listFile.listFiles();
+                    for (File subFile : subFiles) {
+                        LocalDate lastModified = null;
                         try {
-                            lastModified= Files.getLastModifiedTime(Paths.get(subFile.getPath())).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                            lastModified = Files.getLastModifiedTime(Paths.get(subFile.getPath())).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
                         } catch (java.io.IOException e) {
                             e.printStackTrace();
                         }
-                        if(lastModified.plusDays(data_del_after_days).isBefore(LocalDate.now()))
-                        { subFile.delete();
-                        logger.info("Data deleted on " + LocalDate.now());
+                        if (lastModified.plusDays(data_del_after_days).isBefore(LocalDate.now())) {
+                            subFile.delete();
+                            logger.info("Data deleted on " + LocalDate.now());
                         }
                     }
                 }
             }
-            if(del_records_fromDB)
-            {
+            if (del_records_fromDB) {
                 anprService.deleteData(data_del_after_days);
                 atccDataService.deleteData(data_del_after_days);
                 vidsService.deleteData(data_del_after_days);
