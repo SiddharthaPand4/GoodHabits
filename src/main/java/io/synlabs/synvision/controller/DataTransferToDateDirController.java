b4b36a7a3d47f9a9ea58fbcc862afc74f6a0edf5
Sangam Dalal <sangam.dalal@synlabs.io>
2020-08-06 18:30:45 +0530

    data transfer api

---
+++ src/main/java/io/synlabs/synvision/controller/DataTransferToDateDirController.java
@@ -0,0 +1,20 @@
+package io.synlabs.synvision.controller;
+
+import io.synlabs.synvision.service.DataTransferToDateDirService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/data")
+public class DataTransferToDateDirController {
+
+    @Autowired
+    DataTransferToDateDirService dataTransferToDateDirService;
+    @GetMapping("/transfer")
+    public void TransferData(){
+        dataTransferToDateDirService.transfer();
+    }
+
+}
---
+++ src/main/java/io/synlabs/synvision/service/DataTransferToDateDirService.java
@@ -0,0 +1,11 @@
+package io.synlabs.synvision.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataTransferToDateDirService {
+
+    public void transfer() {
+        System.out.println("Code to transfer data here");
+    }
+}
