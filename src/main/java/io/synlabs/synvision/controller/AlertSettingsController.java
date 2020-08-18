a224d66c74eb266b13d3a465b0a53244964c52c8
pandeysiddhartha915 <siddharth@synlabs.io>
2020-08-18 12:42:05 +0530

    new privilege added and added alert settings to admin menu

--- src/main/java/io/synlabs/synvision/controller/AlertSettingsController.java
+++ src/main/java/io/synlabs/synvision/controller/AlertSettingsController.java
@@ -1,5 +1,6 @@
 package io.synlabs.synvision.controller;
 
+import io.synlabs.synvision.auth.SynvisionAuth;
 import io.synlabs.synvision.jpa.VidsAlertSettingRepository;
 import io.synlabs.synvision.service.AlertSettingsService;
 import io.synlabs.synvision.views.AlertSettingsRequest;
@@ -7,9 +8,13 @@ import io.synlabs.synvision.views.AlertSettingsResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static io.synlabs.synvision.auth.SynvisionAuth.Privileges.ALERT_SETTING_READ;
+import static io.synlabs.synvision.auth.SynvisionAuth.Privileges.ALERT_SETTING_WRITE;
 
 @RestController
 @RequestMapping("/api/setting/alerts")
@@ -21,11 +26,13 @@ public class AlertSettingsController {
     private AlertSettingsService alertSettingsService;
 
     @GetMapping("/list")
+    @Secured(ALERT_SETTING_READ)
     public List<AlertSettingsResponse> alertList() {
         return alertSettingsService.alertList();
     }
 
     @PutMapping("/update")
+    @Secured(ALERT_SETTING_WRITE)
     public void saveAlertSettings(@RequestBody List<AlertSettingsRequest> alertSettingsRequestList) {
         alertSettingsService.saveAlertSettings(alertSettingsRequestList);
     }
