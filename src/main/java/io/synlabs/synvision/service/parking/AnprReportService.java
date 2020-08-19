be1e4a07aaf73eb74cf26506bd75cb4ba88980bc
pandeysiddhartha915 <siddharth@synlabs.io>
2020-08-19 11:23:36 +0530

    added null check for feedId

--- src/main/java/io/synlabs/synvision/service/parking/AnprReportService.java
+++ src/main/java/io/synlabs/synvision/service/parking/AnprReportService.java
@@ -58,7 +58,7 @@ public class AnprReportService extends BaseService {
         query.select(anprEvent)
                 .from(anprEvent);
 
-        if (request.feedId != 0) query.where(anprEvent.feed.id.eq(request.feedId));
+        if (request.feedId != 0 && request.getFeedId() != null) query.where(anprEvent.feed.id.eq(request.feedId));
 
         if (!StringUtils.isEmpty(request.getLpr())) {
             query.where(anprEvent.anprText.like("%" + request.getLpr() + "%"));
