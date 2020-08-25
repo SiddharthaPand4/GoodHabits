1c1398d08b15c3acfb658bc47ff3f104db413903
pandeysiddhartha915 <siddharth@synlabs.io>
2020-08-25 15:52:45 +0530

    dashboard image  change according to feed filter

--- src/main/java/io/synlabs/synvision/jpa/HighwayTrafficStateRepository.java
+++ src/main/java/io/synlabs/synvision/jpa/HighwayTrafficStateRepository.java
@@ -1,9 +1,14 @@
 package io.synlabs.synvision.jpa;
 
+import io.synlabs.synvision.entity.core.Feed;
 import io.synlabs.synvision.entity.vids.HighwayTrafficState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HighwayTrafficStateRepository extends JpaRepository<HighwayTrafficState, Long> {
 
     HighwayTrafficState findFirstByOrderByUpdateDateDesc();
+
+    HighwayTrafficState findFirstByFeedOrderByUpdateDateDesc(Feed feed);
 }
