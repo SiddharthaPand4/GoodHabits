ce0b7a8bd9e603907f7c9566240a2a9e70411fde
vikumarsing <vikumarsing@synlabs.io>
2020-09-09 12:43:05 +0530

    org-branding-fixes

--- src/main/web/client/src/components/Footerbar.js
+++ src/main/web/client/src/components/Footerbar.js
@@ -9,7 +9,7 @@ export default class Headbar extends Component {
         return (
             <Footer>
                 <Row>
-                    <Col span={12} style={{textAlign: 'left'}}><span >Provided by SynergyLabs Technology</span></Col>
+                    <Col span={12} style={{textAlign: 'left'}}><span style={{color: "#f0f2f5"}}>Provided by SynergyLabs Technology</span></Col>
                     <Col span={12} style={{textAlign: 'right'}}><span>Powered by {this.props.org.legalName}</span></Col>
                 </Row>
             </Footer>
